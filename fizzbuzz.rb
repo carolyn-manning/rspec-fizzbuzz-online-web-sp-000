cd34d414566f60945e3c45b8b4f303ba29516914
Carolyn Manning <carolyn.manning3@gmail.com>
2020-09-10 02:58:49 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,6 +2,7 @@ def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
   elsif int % 5 == 0
-    "Buzz"  
+    "Buzz"
+  elsif if int % 3 == 0 && int % 5 == 0
   end
 end
